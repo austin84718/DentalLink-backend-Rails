95049fa2aee07781951bf06831d1ddb2e0dbda36
Anton Mezerny <mezerny@gmail.com>
2014-01-29 19:14:03 +0200

    Routes improvement

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,7 @@
 ReferralServerRuby::Application.routes.draw do
   #devise_for :users, controllers: {sessions:'sessions', registrations: 'registrations', passwords: 'passwords'}, token_authentication_key: 'authentication_token'
 
-  devise_for :users, controllers: {sessions:'sessions', registrations: 'registrations', passwords: 'passwords'}, skip: [:sessions, :registrations, :passwords, :confirmations], token_authentication_key: 'authentication_token'
-  as :user do
-    post '/sign_in' => 'sessions#create', as: :user_session
-    delete '/sign_out' => 'sessions#destroy', as: :destroy_user_session
-    get '/sign_in' => 'sessions#new', as: :new_user_session
-
-    post '/confirm' => 'devise/confirmations#create'
-    get '/confirm/new' => 'devise/confirmations#new', as: :new_user_confirmation
-    get '/confirm' => 'devise/confirmations#show', as: :user_confirmation
-
-    get '/sign_up' => 'registrations#new', as: :new_user_registration
-    post '/sign_up' => 'registrations#create'
-    put '/users' => 'registrations#update'
-    delete '/users' => 'registrations#destroy'
-
-    post '/password' => 'passwords#create'
-    get '/password' => 'passwords#new', as: :new_user_password
-    get '/password/edit' => 'passwords#edit', as: :edit_user_password
-  end
-
-
+  devise_for :users, path: '', path_names: {  registration: 'registration'}, controllers: {sessions: :sessions, registrations: :registrations, passwords: :passwords}, token_authentication_key: 'authentication_token'
 
   resources :addresses
 
@@ -31,6 +11,7 @@ ReferralServerRuby::Application.routes.draw do
 
   resources :practices
 
+  #we don't need users as a resourceful route, since we have authentication framework that handles registrations and other things
   #resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
