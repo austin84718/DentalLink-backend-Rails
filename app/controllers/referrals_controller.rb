86156567bde883c0b201c10cafabeba77d23c80d
Anton Mezerny <mezerny@gmail.com>
2014-06-10 16:42:24 +0300

    add target practice contact information in PDF

--- app/controllers/referrals_controller.rb
+++ app/controllers/referrals_controller.rb
@@ -23,7 +23,13 @@ class ReferralsController < ApplicationController
   # GET /referrals/1.json
   def show
     respond_to do |format|
-      format.json { render json: @referral, include: {patient: {}, attachments: {}, procedure: {include: :practice_type}, orig_provider: {include: :practice}, dest_provider: {include: {practice: {include: :users}}}, notes: {include: :user}} }
+      format.json { render json: @referral, include: {
+          patient: {},
+          attachments: {},
+          procedure: {include: :practice_type},
+          orig_provider: {include: :practice},
+          dest_provider: {include: {practice: {include: {users: {}, address: {}}}}},
+          notes: {include: :user}} }
     end
   end
 
